1e208bd145a95ae0cc5ae497ecf47f1257c5b2d8
rahuldkjain <rahuldkjain@gmail.com>
2020-07-15 17:18:00 +0530

    Updates

--- src/components/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Footer = () => {
-    return (
-        <div className="footer">
-            Developed with ❤️ by <a href="https://rahuldkjain.github.io" style={{textDecoration: 'none'}}>Rahul</a>
-        </div>
-    )
-}
-export default Footer;
---
+++ styles-1211262dd08146354a2f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],[]]);
+//# sourceMappingURL=styles-1211262dd08146354a2f.js.map
