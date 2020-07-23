38f7a84b7a061ab44f08de666fefc0905a3cbd9e
Prakash Chokalingam <cpvicky33@gmail.com>
2020-07-23 20:58:35 +0530

    Github stats

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,25 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { StarIcon, RepoForkedIcon } from '@primer/octicons-react'
 import logo from '../images/mdg.png'
 import gsap from 'gsap';
 
 const Header = (props) => {
-  useEffect(()=>{
+  const [stats, setStats] = useState({ stars: 0, forks: 0 });
+
+  const fetchStats = async () => {
+    console.log('stats:updated')
+    const response = await fetch('https://api.github.com/repos/rahuldkjain/github-profile-readme-generator');
+    if (response.ok) {
+      const {forks_count: forks, stargazers_count: stars } = await response.json();
+      setStats({ stars, forks });
+    } else {
+      throw 'Api fetching failed'
+    }
+  }
+
+  const showStat = (count) => count ? `(${count})` : '';
+
+  useEffect(()=> {
     gsap.set('.star-icon, .fork-icon', {
       transformOrigin: 'center',
     });
@@ -15,7 +30,13 @@ const Header = (props) => {
       repeat: -1,
       yoyo: true,
     });
-  })
+
+    // repo stats
+    fetchStats(); // async initial
+    window.addEventListener('visibilitychange', () => !document.hidden && window.setTimeout(fetchStats, 5000)); // post 5 secs
+    window.setInterval(fetchStats, 900000); // every 15 mins
+  }, [])
+
   return (
     <div className="header">
       <h1 className="heading">
@@ -28,11 +49,11 @@ const Header = (props) => {
       <div className="github">
         <a href="https://github.com/rahuldkjain/github-profile-readme-generator"
           aria-label="Star rahuldkjain/github-profile-readme-generator on GitHub" target="blank">
-          <div className="github-button"> <StarIcon size={16} className="star-icon" /> Star this repo </div>
+          <div className="github-button"> <StarIcon size={16} className="star-icon" /> Star this repo {showStat(stats.stars)} </div>
         </a>
         <a href="https://github.com/rahuldkjain/github-profile-readme-generator/fork"
           aria-label="Fork rahuldkjain/github-profile-readme-generator on GitHub" target="blank">
-          <div className="github-button"><RepoForkedIcon size={16} className="fork-icon" /> Fork on Github </div>
+          <div className="github-button"><RepoForkedIcon size={16} className="fork-icon" /> Fork on Github {showStat(stats.forks)} </div>
         </a>
       </div>
     </div>
