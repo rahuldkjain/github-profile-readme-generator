a49978abeec242265f17417d94ae7debb555e4d9
Ankur Chaudhary <ankurchaudhary627@gmail.com>
2020-07-24 22:39:46 +0530

    Made the required changes for the PR

--- src/components/header.js
+++ src/components/header.js
@@ -1,60 +1,81 @@
 import React, { useEffect, useState } from "react"
-import { StarIcon, RepoForkedIcon } from '@primer/octicons-react'
-import logo from '../images/mdg.png'
-import gsap from 'gsap';
-import axios from 'axios';
+import { StarIcon, RepoForkedIcon } from "@primer/octicons-react"
+import logo from "../images/mdg.png"
+import gsap from "gsap"
+import axios from "axios"
 
-const Header = (props) => {
+const Header = props => {
   const fetchData = async () => {
-    var response = await axios.get('https://api.github.com/repos/rahuldkjain/github-profile-readme-generator');
-    
-    const {stargazers_count,forks_count} = response.data;
-    
+    var response = await axios.get(
+      "https://api.github.com/repos/rahuldkjain/github-profile-readme-generator"
+    )
+
+    const { stargazers_count, forks_count } = response.data
+
     setstats({
-      starsCount : stargazers_count,
-      forksCount : forks_count
-    });
+      starsCount: stargazers_count,
+      forksCount: forks_count,
+    })
   }
 
   const [stats, setstats] = useState({
-    starsCount : 0,
-    forksCount : 0
-  });          
+    starsCount: 0,
+    forksCount: 0,
+  })
 
   useEffect(() => {
-    fetchData();
-    setInterval( fetchData, 15000);
-  }, []);
+    fetchData()
+    setInterval(fetchData, 60000)
 
-  useEffect(()=>{
-    gsap.set('.star-icon, .fork-icon', {
-      transformOrigin: 'center',
-    });
-    gsap.to('.star-icon, .fork-icon', {
-      rotateZ: '360',
+    gsap.set(".star-icon, .fork-icon", {
+      transformOrigin: "center",
+    })
+    gsap.to(".star-icon, .fork-icon", {
+      rotateZ: "360",
       duration: 2,
-      ease: 'elastic.inOut',
+      ease: "elastic.inOut",
       repeat: -1,
       yoyo: true,
-    });
-  })
+    })
+  }, [])
+
   return (
     <div className="header">
       <h1 className="heading">
-        <img src={logo} onClick={() => window.location.reload(false)}
-          className="logo" alt="github profile markdown generator logo" />
-        <div onClick={() => window.location.reload(false)} role="button" tabIndex="0">
+        <img
+          src={logo}
+          onClick={() => window.location.reload(false)}
+          className="logo"
+          alt="github profile markdown generator logo"
+        />
+        <div
+          onClick={() => window.location.reload(false)}
+          role="button"
+          tabIndex="0"
+        >
           {props.heading}
         </div>
       </h1>
-      <div className="github" >
-        <a href="https://github.com/rahuldkjain/github-profile-readme-generator"
-          aria-label="Star rahuldkjain/github-profile-readme-generator on GitHub" target="blank">
-          <div className="github-button" style={{width:"90%"}}> <StarIcon size={16} className="star-icon" /> Star this repo { stats.starsCount }</div>
+      <div className="github">
+        <a
+          href="https://github.com/rahuldkjain/github-profile-readme-generator"
+          aria-label="Star rahuldkjain/github-profile-readme-generator on GitHub"
+          target="blank"
+        >
+          <div className="github-button">
+            <StarIcon size={16} className="star-icon" /> Star this repo{" "}
+            {stats.starsCount}
+          </div>
         </a>
-        <a href="https://github.com/rahuldkjain/github-profile-readme-generator/fork"
-          aria-label="Fork rahuldkjain/github-profile-readme-generator on GitHub" target="blank">
-          <div className="github-button" style={{width:"90%"}}><RepoForkedIcon size={16} className="fork-icon" /> Fork on Github { stats.forksCount }</div>
+        <a
+          href="https://github.com/rahuldkjain/github-profile-readme-generator/fork"
+          aria-label="Fork rahuldkjain/github-profile-readme-generator on GitHub"
+          target="blank"
+        >
+          <div className="github-button">
+            <RepoForkedIcon size={16} className="fork-icon" /> Fork on Github{" "}
+            {stats.forksCount}
+          </div>
         </a>
       </div>
     </div>
