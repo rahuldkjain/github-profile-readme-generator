36f75afb90062f98d1ad6107dfc71be6d0a4ad72
Mauricio Hernan Cabrera <6373415-hernandracoder@users.noreply.gitlab.com>
2020-08-23 10:12:16 -0300

    subtitle components code improved

--- src/components/subtitle.js
+++ src/components/subtitle.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react"
 
-const Subtitle = (props) => {
-    return (
-        <div className="section">
-            <div className="section-title">Subtitle</div>
-            <input id="subtitle" className="inputField lg prefix" value={props.data.subtitle} 
-            onChange={event => props.handleDataChange('subtitle', event)} />
-        </div>
-    );
+const Subtitle = props => {
+  return (
+    <div className="section">
+      <div className="section-title">Subtitle</div>
+      <input
+        id="subtitle"
+        className="inputField lg prefix"
+        value={props.data.subtitle}
+        onChange={event => props.handleDataChange("subtitle", event)}
+      />
+    </div>
+  )
 }
 
-export default Subtitle;
+export default Subtitle
