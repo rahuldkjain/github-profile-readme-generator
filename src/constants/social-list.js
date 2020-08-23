8a202056549840fcdb9e6f428b62340dcceaa740
Mauricio Hernan Cabrera <6373415-hernandracoder@users.noreply.gitlab.com>
2020-08-23 09:58:19 -0300

    markdown-preview components code improved

---
+++ src/constants/social-list.js
@@ -0,0 +1,72 @@
+const socialList = [
+  {
+    base: "https://codepen.io",
+    icon: "https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/codepen.svg",
+    key: "codepen",
+  },
+  {
+    base: "https://dev.to",
+    icon:
+      "https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/dev-dot-to.svg",
+    username: "dev",
+  },
+  {
+    base: "https://twitter.com",
+    icon: "https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/twitter.svg",
+    username: "twitter",
+  },
+  {
+    base: "https://linkedin.com/in",
+    icon: "https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/linkedin.svg",
+    username: "linkedin",
+  },
+  {
+    base: "https://stackoverflow.com/users",
+    icon:
+      "https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/stackoverflow.svg",
+    username: "stackoverflow",
+  },
+  {
+    base: "https://codesandbox.com",
+    icon:
+      "https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/codesandbox.svg",
+    username: "codesandbox",
+  },
+  {
+    base: "https://kaggle.com",
+    icon: "https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/kaggle.svg",
+    username: "kaggle",
+  },
+  {
+    base: "https://fb.com",
+    icon: "https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/facebook.svg",
+    username: "fb",
+  },
+  {
+    base: "https://instagram.com",
+    icon: "https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/instagram.svg",
+    username: "instagram",
+  },
+  {
+    base: "https://dribbble.com",
+    icon: "https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/dribbble.svg",
+    username: "dribbble",
+  },
+  {
+    base: "https://www.behance.net",
+    icon: "https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/behance.svg",
+    username: "behance",
+  },
+  {
+    base: "https://medium.com",
+    icon: "https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/medium.svg",
+    username: "medium",
+  },
+  {
+    base: "https://www.youtube.com/c",
+    icon: "https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/youtube.svg",
+    username: "youtube",
+  },
+]
+
+export default socialList
