5cb24ab51b23b97d084ba5da6b2bd319ddbdd612
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2020-08-23 10:12:31 -0300

    title components code improved

--- src/components/title.js
+++ src/components/title.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React from "react"
 
-const Title = (props) => {
-    return (
-        <div className="section">
-            <div className="section-title">Title</div>
-            <input id="title-prefix" className="inputField sm prefix" value={props.prefix.title} 
-            onChange={event => props.handlePrefixChange('title', event)} />
-            <input id="title-name" placeholder="name" className="inputField md" value={props.data.title} 
-            onChange={event => props.handleDataChange('title', event)} />
-        </div>
-    );
+const Title = props => {
+  return (
+    <div className="section">
+      <div className="section-title">Title</div>
+      <input
+        id="title-prefix"
+        className="inputField sm prefix"
+        value={props.prefix.title}
+        onChange={event => props.handlePrefixChange("title", event)}
+      />
+      <input
+        id="title-name"
+        placeholder="name"
+        className="inputField md"
+        value={props.data.title}
+        onChange={event => props.handleDataChange("title", event)}
+      />
+    </div>
+  )
 }
 
-export default Title;
+export default Title
