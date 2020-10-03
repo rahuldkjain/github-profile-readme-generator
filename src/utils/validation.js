effa02668aed19717fa002f03fe99a445e0e1d47
Sunit Shirke <sunitshirke88@gmail.com>
2020-10-03 18:07:36 +0530

    🔒 Updated regex for Twitter Username Validation 🔒

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -9,4 +9,9 @@ const isMediumUsernameValid = username => {
   return true
 }
 
-export { isGitHubUsernameValid, isMediumUsernameValid }
+const isTwitterUsernameValid = username => {
+  var pattern = /^[a-zA-Z0-9_]{1,15}$/
+  return pattern.test(username)
+}
+
+export { isGitHubUsernameValid, isMediumUsernameValid, isTwitterUsernameValid }
