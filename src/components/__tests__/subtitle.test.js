a1bbb7f88761f0a2e18c77aeadec47c598561b02
Citrinin <kate.dainovich@gmail.com>
2020-10-25 17:06:57 +0300

    make tests green

--- src/components/__tests__/subtitle.test.js
+++ src/components/__tests__/subtitle.test.js
@@ -5,11 +5,22 @@ import toJson from "enzyme-to-json"
 import Subtitle from "../subtitle"
 
 describe("Subtitle", () => {
-  const component = shallow(
-    <Subtitle data={{ subtitle: "A frontend developer" }} />
-  )
+  const mockEvent = { target: { value: "This is a mock event" } }
+  const props = {
+    data: {
+      subtitle: "subtitle",
+    },
+    handleDataChange: jest.fn().mockReturnValue({}),
+  }
+
+  const component = shallow(<Subtitle {...props} />)
 
   it("renders correctly", () => {
     expect(toJson(component)).toMatchSnapshot()
+  })
+
+  it("calls onChange", () => {
+    component.find("input").at(0).simulate("change", mockEvent)
+    expect(props.handleDataChange).toBeCalledWith("subtitle", mockEvent)
   })
 })
