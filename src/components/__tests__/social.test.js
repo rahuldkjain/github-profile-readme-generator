07b18b4c8a061cfe884ee89509c2b3a5d3fe4822
Tekena Solomon <tekena157@gmail.com>
2020-10-26 20:47:40 +0100

    revert all other changes not related to social component

--- src/components/__tests__/social.test.js
+++ src/components/__tests__/social.test.js
@@ -1,13 +1,44 @@
 import React from "react"
-import renderer from "react-test-renderer"
+import { shallow } from "enzyme"
+import toJson from "enzyme-to-json"
 
 import Social from "../social"
 
 describe("Social", () => {
+  const mockEvent = { target: { value: "This is a mock event" } }
+  const props = {
+    social: {
+      github: "github ",
+      twitter: "twitter",
+      dev: "dev",
+      codepen: "codepen",
+      codesandbox: "codesandbodx",
+      stackoverflow: "stackoverflow",
+      linkedin: "linkedin",
+      kaggle: "kaggle",
+      fb: "fb",
+      instagram: "instagram",
+      dribble: "dribble",
+      behance: "behance",
+      medium: "medium",
+      youtube: "youtube",
+      codechef: "codechef",
+      hackerrack: "hackerranck",
+      codeforces: "codeforces",
+      leetcode: "leetcode",
+      topcoder: "topcoder",
+      hackerearth: "@hackerearth",
+      geeks_for_geeks: "geeks_for_geeks",
+      discord: "discord",
+      rssurl: "rssurl",
+    },
+    handleSocialChange: jest.fn().mockReturnValue({}),
+  }
   it("renders correctly", () => {
-    const tree = renderer
-      .create(<Social social={{ github: "github" }} />)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
+    const component = shallow(<Social {...props} />)
+    for (let i = 0; i < component.find("input").length; i++) {
+      component.find("input").at(i).simulate("change", mockEvent)
+    }
+    expect(toJson(component)).toMatchSnapshot()
   })
 })
