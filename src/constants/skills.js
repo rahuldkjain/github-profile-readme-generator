0b70050da043060e466f24fe427c079c2a645f93
The Blob SCP <81673375+theblobscp@users.noreply.github.com>
2021-07-16 19:39:33 -0400

    Update skills.js with jquery stuff

--- src/constants/skills.js
+++ src/constants/skills.js
@@ -51,6 +51,7 @@ const categorizedSkills = {
       "qt",
       "wx_widgets",
       "ember",
+      "jquery"
     ],
   },
 
@@ -441,6 +442,7 @@ const icons = {
   middleman:
     "https://raw.githubusercontent.com/leungwensen/svg-icon/b84b3f3a3da329b7c1d02346865f8e98beb05413/dist/svg/logos/middleman.svg",
   matlab: "https://upload.wikimedia.org/wikipedia/commons/2/21/Matlab_Logo.png",
+  jquery: "https://raw.githubusercontent.com/devicons/devicon/develop/icons/jquery/jquery-original.svg",
 }
 
 const skillWebsites = {
@@ -591,6 +593,7 @@ const skillWebsites = {
   postman: "https://postman.com",
   erlang: "https://www.erlang.org/",
   middleman: "https://middlemanapp.com/",
+  jquery: "https://jquery.com/",
 }
 
 const initialSkillState = {}
