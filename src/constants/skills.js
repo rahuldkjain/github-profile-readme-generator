61b8362a554f1b855cb1da4499539a58f6db6f27
Pranansh Singh <43909197+pranansh-s@users.noreply.github.com>
2021-10-06 02:54:21 +0530

    Added Nim-Programming language

--- src/constants/skills.js
+++ src/constants/skills.js
@@ -22,6 +22,7 @@ const categorizedSkills = {
       "coffeescript",
       "elixir",
       "erlang",
+      "nim",
     ],
   },
 
@@ -436,6 +437,7 @@ const icons = {
   zapier: "https://www.vectorlogo.zone/logos/zapier/zapier-icon.svg",
   postman: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
   erlang: "https://www.vectorlogo.zone/logos/erlang/erlang-official.svg",
+  nim: "https://nim-lang.org/assets/img/logo.svg",
   sqlite: "https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg",
   mssql: "https://www.svgrepo.com/show/303229/microsoft-sql-server-logo.svg",
   middleman:
@@ -590,6 +592,7 @@ const skillWebsites = {
   mssql: "https://www.microsoft.com/en-us/sql-server",
   postman: "https://postman.com",
   erlang: "https://www.erlang.org/",
+  nim: "https://nim-lang.org/",
   middleman: "https://middlemanapp.com/",
 }
 
